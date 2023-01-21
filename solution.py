e73658480ff022286f47fdfc97b478da71813f2b
Alexey Shapovalov <ascket@mail.ru>
2023-01-21 22:35:22 +0100

    Homework solution

---
+++ solution.py
@@ -0,0 +1,37 @@
+import json, csv
+from files import JSON_FILE_PATH
+from files import CSV_FILE_PATH
+from files import get_path
+
+users_with_books = []
+
+with open(JSON_FILE_PATH) as file:
+    users = json.loads(file.read())
+
+with open(CSV_FILE_PATH) as file:
+    books = csv.reader(file)
+    header = next(books)
+    for x in range(len(users)):
+        users_with_books.append({
+            "name": users[x]["name"],
+            "gender": users[x]["gender"],
+            "address": users[x]["address"],
+            "age": users[x]["age"],
+            "books": []
+        })
+    while True:
+        try:
+            for x in range(len(users_with_books)):
+                book = next(books)
+                users_with_books[x]["books"].append({
+                    "title": book[0],
+                    "author": book[1],
+                    "pages": book[3],
+                    "genre": book[2]
+                })
+        except StopIteration:
+            break
+
+with open(get_path("result2.json"), "w") as f:
+    s = json.dumps(users_with_books, indent=4)
+    f.write(s)
